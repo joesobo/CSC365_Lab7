329f8ec79ae3747ac7b8bb15beaf8f3408f48b55
Joe Soboleski <soboleskijoe@gmail.com>
2021-03-17 16:15:23 -0700

    a little more formatting

--- src/main/java/InnReservations.java
+++ src/main/java/InnReservations.java
@@ -1,3 +1,5 @@
+import static java.time.temporal.ChronoUnit.DAYS;
+
 import java.io.FileNotFoundException;
 import java.sql.*;
 import java.sql.Date;
@@ -8,8 +10,6 @@ import java.time.LocalDate;
 import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
-
-import static java.time.temporal.ChronoUnit.DAYS;
 
 public class InnReservations {
 
@@ -90,23 +90,23 @@ public class InnReservations {
     DateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy");
     String query =
       "with lastCheckin as (\n" +
-      "select room, max(checkin) as maxCheckin\n" +
-      "from lab7_reservations\n" +
-      "where checkin <= curdate()\n" +
-      "group by room\n" +
+        "select room, max(checkin) as maxCheckin\n" +
+        "from lab7_reservations\n" +
+        "where checkin <= curdate()\n" +
+        "group by room\n" +
       "), soonestReservation as (\n" +
-      "select res.room, dateadd(day, 1, checkout) as dayAfterReservation\n" +
-      "from lab7_reservations as res\n" +
-      "join lastCheckin as lc on res.room = lc.room\n" +
-      "where\n" +
-      "maxCheckin <= checkin and\n" +
-      "dateadd(day, 1, checkout) not in (\n" +
-      "select checkin from lab7_reservations\n" +
-      ")\n" +
+        "select res.room, dateadd(day, 1, checkout) as dayAfterReservation\n" +
+        "from lab7_reservations as res\n" +
+        "join lastCheckin as lc on res.room = lc.room\n" +
+        "where\n" +
+        "maxCheckin <= checkin and\n" +
+        "dateadd(day, 1, checkout) not in (\n" +
+          "select checkin from lab7_reservations\n" +
+        ")\n" +
       "), nextReservations as (\n" +
-      "select room, checkin\n" +
-      "from lab7_reservations\n" +
-      "where curdate() <= checkin\n" +
+        "select room, checkin\n" +
+        "from lab7_reservations\n" +
+        "where curdate() <= checkin\n" +
       ")\n" +
       "select\n" +
       "roomcode,\n" +
@@ -117,9 +117,9 @@ public class InnReservations {
       "basePrice,\n" +
       "decor,\n" +
       "case when\n" +
-      "curdate() >= ifnull(min(sr.dayAfterReservation), curdate())\n" +
-      "then null\n" +
-      "else min(sr.dayAfterReservation)\n" +
+        "curdate() >= ifnull(min(sr.dayAfterReservation), curdate())\n" +
+        "then null\n" +
+        "else min(sr.dayAfterReservation)\n" +
       "end as nextAvailable,\n" +
       "min(nr.checkin) as nextReservation\n" +
       "from lab7_rooms as r\n" +
@@ -140,7 +140,7 @@ public class InnReservations {
         ResultSet rs = statement.executeQuery(query)
       ) {
         System.out.println("Rooms and Rates:");
-        System.out.print(heading);
+        System.out.print(heading.toUpperCase());
 
         while (rs.next()) {
           Date nextAvailable = rs.getDate("nextavailable");
@@ -181,7 +181,6 @@ public class InnReservations {
   }
 
   private static void makeReservation(Scanner in) {
-
     System.out.println("Make a new Reservation");
     in.nextLine();
 
@@ -206,16 +205,36 @@ public class InnReservations {
     System.out.println("Number of Adults(18+): ");
     int adults = in.nextInt();
 
-    System.out.println("Checking availability for "+roomCode+" between "+checkIn+" and "+checkOut+" for "+children+" children and "+adults+" adults");
+    System.out.println(
+      "Checking availability for " +
+      roomCode +
+      " between " +
+      checkIn +
+      " and " +
+      checkOut +
+      " for " +
+      children +
+      " children and " +
+      adults +
+      " adults"
+    );
 
-    try (Connection conn = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD)) {
+    try (
+      Connection conn = DriverManager.getConnection(
+        JDBC_URL,
+        JDBC_USER,
+        JDBC_PASSWORD
+      )
+    ) {
+      String sqlInsertString =
+        "INSERT INTO lab7_reservations " +
+        "(CODE, Room, CheckIn, CheckOut, Rate, LastName, FirstName, Adults, Kids) " +
+        "VALUES ((SELECT max(CODE) + 1 FROM lab7_reservations), ?, ?, ?, " +
+        "(SELECT basePrice FROM lab7_rooms where RoomCode = ?), ?, ?, ?, ?)";
 
-      String sqlInsertString = "INSERT INTO lab7_reservations " + 
-      "(CODE, Room, CheckIn, CheckOut, Rate, LastName, FirstName, Adults, Kids) " +
-      "VALUES ((SELECT max(CODE) + 1 FROM lab7_reservations), ?, ?, ?, " +
-      "(SELECT basePrice FROM lab7_rooms where RoomCode = ?), ?, ?, ?, ?)";
-
-      try (PreparedStatement pStatement = conn.prepareStatement(sqlInsertString)) {
+      try (
+        PreparedStatement pStatement = conn.prepareStatement(sqlInsertString)
+      ) {
         pStatement.setString(1, roomCode);
         pStatement.setDate(2, checkIn);
         pStatement.setDate(3, checkOut);
@@ -226,16 +245,20 @@ public class InnReservations {
         pStatement.setInt(8, children);
 
         if (pStatement.executeUpdate() > 0) {
+          String sqlConfirmationString =
+            "SELECT CODE, FirstName, LastName, RoomCode, RoomName, bedType, CheckIn, " +
+            "CheckOut, Adults, kids, Rate FROM lab7_reservations JOIN lab7_rooms on Room = RoomCode " +
+            "WHERE CheckIn = ? and CheckOut = ? and Room = ?";
 
-          String sqlConfirmationString = "SELECT CODE, FirstName, LastName, RoomCode, RoomName, bedType, CheckIn, " +
-          "CheckOut, Adults, kids, Rate FROM lab7_reservations JOIN lab7_rooms on Room = RoomCode " +
-          "WHERE CheckIn = ? and CheckOut = ? and Room = ?";
-
-          try (PreparedStatement confirmiationPStatement = conn.prepareStatement(sqlConfirmationString)) {
-            confirmiationPStatement.setDate(1, checkIn);
-            confirmiationPStatement.setDate(2, checkOut);
-            confirmiationPStatement.setString(3, roomCode);
-            ResultSet resultSet = confirmiationPStatement.executeQuery();
+          try (
+            PreparedStatement confirmationPStatement = conn.prepareStatement(
+              sqlConfirmationString
+            )
+          ) {
+            confirmationPStatement.setDate(1, checkIn);
+            confirmationPStatement.setDate(2, checkOut);
+            confirmationPStatement.setString(3, roomCode);
+            ResultSet resultSet = confirmationPStatement.executeQuery();
 
             if (resultSet.next()) {
               String confirmationCode = resultSet.getString("CODE");
@@ -244,30 +267,59 @@ public class InnReservations {
               String confirmRoomCode = resultSet.getString("RoomCode");
               String confirmRoomName = resultSet.getString("RoomName");
               String confirmBedType = resultSet.getString("bedType");
-              LocalDate confirmCheckIn = resultSet.getDate("CheckIn").toLocalDate();
-              LocalDate confirmCheckOut = resultSet.getDate("CheckOut").toLocalDate();
+              LocalDate confirmCheckIn = resultSet
+                .getDate("CheckIn")
+                .toLocalDate();
+              LocalDate confirmCheckOut = resultSet
+                .getDate("CheckOut")
+                .toLocalDate();
               int confirmAdults = resultSet.getInt("Adults");
               int confirmKids = resultSet.getInt("Kids");
               int rate = resultSet.getInt("Rate");
               long totalDays = DAYS.between(confirmCheckIn, confirmCheckOut);
-              Predicate<LocalDate> isWeekend = date -> date.getDayOfWeek() == DayOfWeek.SATURDAY
-                || date.getDayOfWeek() == DayOfWeek.SUNDAY;
-              long weekendDays = Stream.iterate(confirmCheckIn, date -> date.plusDays(1)).limit(totalDays)
-                .filter(isWeekend).count();
+              Predicate<LocalDate> isWeekend = date ->
+                date.getDayOfWeek() == DayOfWeek.SATURDAY ||
+                date.getDayOfWeek() == DayOfWeek.SUNDAY;
+              long weekendDays = Stream
+                .iterate(confirmCheckIn, date -> date.plusDays(1))
+                .limit(totalDays)
+                .filter(isWeekend)
+                .count();
               long weekDays = totalDays - weekendDays;
               double totalCharge = rate * weekDays + (rate * 1.1) * weekendDays;
-              
-              System.out.println("Confirmation\n\nReservation Code :" + confirmationCode + "\n" +
-                confirmFirstName + " " + confirmLastName + "\n" +
-                confirmRoomName + " (" + confirmRoomCode + "), " + confirmBedType + " bed\n" +
-                confirmAdults + " Number of Adults, and " + confirmKids + " Children\n\n" +
-                "Total Cost for " + totalDays + " is: " + totalCharge + "$\n\n");
+
+              System.out.println(
+                "Confirmation\n\nReservation Code :" +
+                confirmationCode +
+                "\n" +
+                confirmFirstName +
+                " " +
+                confirmLastName +
+                "\n" +
+                confirmRoomName +
+                " (" +
+                confirmRoomCode +
+                "), " +
+                confirmBedType +
+                " bed\n" +
+                confirmAdults +
+                " Number of Adults, and " +
+                confirmKids +
+                " Children\n\n" +
+                "Total Cost for " +
+                totalDays +
+                " is: " +
+                totalCharge +
+                "$\n\n"
+              );
             }
           }
         }
       }
     } catch (SQLException e) {
-      System.err.println("Could not create reservation: " + e.getMessage().split(";")[0] + "\n");
+      System.err.println(
+        "Could not create reservation: " + e.getMessage().split(";")[0] + "\n"
+      );
     }
   }
 
@@ -279,7 +331,11 @@ public class InnReservations {
     int reservationCode = in.nextInt();
     in.nextLine();
 
-    System.out.println("\n***If you do not want to change a field please enter " + noChange + "***\n");
+    System.out.println(
+      "\n***If you do not want to change a field please enter " +
+      noChange +
+      "***\n"
+    );
     System.out.println("New First Name:");
     String firstName = in.nextLine().toUpperCase();
 
@@ -299,30 +355,32 @@ public class InnReservations {
     String adultsString = in.nextLine();
 
     List<Object> values = new ArrayList<>();
-    StringBuilder sqlQueryBuilder = new StringBuilder("UPDATE lab7_reservations SET ");
+    StringBuilder sqlQueryBuilder = new StringBuilder(
+      "UPDATE lab7_reservations SET "
+    );
     StringJoiner stringJoiner = new StringJoiner(", ");
 
-    if (!firstName.toUpperCase().equals(noChange)) {
+    if (!firstName.equalsIgnoreCase(noChange)) {
       stringJoiner.add("FirstName = ?");
       values.add(firstName);
     }
-    if (!lastName.toUpperCase().equals(noChange)) {
+    if (!lastName.equalsIgnoreCase(noChange)) {
       stringJoiner.add("LastName = ?");
       values.add(lastName);
     }
-    if (!checkInString.toUpperCase().equals(noChange)) {
+    if (!checkInString.equalsIgnoreCase(noChange)) {
       stringJoiner.add("CheckIn = ?");
       values.add(Date.valueOf(checkInString));
     }
-    if (!checkOutString.toUpperCase().equals(noChange)) {
+    if (!checkOutString.equalsIgnoreCase(noChange)) {
       stringJoiner.add("CheckOut = ?");
       values.add(Date.valueOf(checkOutString));
     }
-    if (!childrenString.toUpperCase().equals(noChange)) {
+    if (!childrenString.equalsIgnoreCase(noChange)) {
       stringJoiner.add("Kids = ?");
       values.add(Integer.valueOf(childrenString));
     }
-    if (!adultsString.toUpperCase().equals(noChange)) {
+    if (!adultsString.equalsIgnoreCase(noChange)) {
       stringJoiner.add("Adults = ?");
       values.add(Integer.valueOf(adultsString));
     }
@@ -330,28 +388,42 @@ public class InnReservations {
     sqlQueryBuilder.append(stringJoiner.toString());
     sqlQueryBuilder.append(" WHERE CODE = ?");
 
-    try (Connection conn = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD)) {
-      
-      try (PreparedStatement pStatement = conn.prepareStatement(sqlQueryBuilder.toString())) {
+    try (
+      Connection conn = DriverManager.getConnection(
+        JDBC_URL,
+        JDBC_USER,
+        JDBC_PASSWORD
+      )
+    ) {
+      try (
+        PreparedStatement pStatement = conn.prepareStatement(
+          sqlQueryBuilder.toString()
+        )
+      ) {
         for (int i = 0; i < values.size(); i++) {
           Object currentObject = values.get(i);
 
           if (currentObject instanceof Integer) {
-            pStatement.setInt(i + 1, (int)currentObject);
+            pStatement.setInt(i + 1, (int) currentObject);
           } else if (currentObject instanceof String) {
-            pStatement.setString(i + 1, (String)currentObject);
+            pStatement.setString(i + 1, (String) currentObject);
           } else if (currentObject instanceof Date) {
-            pStatement.setDate(i + 1, (Date)currentObject);
+            pStatement.setDate(i + 1, (Date) currentObject);
           }
         }
 
         pStatement.setInt(values.size() + 1, reservationCode);
         long changeCount = pStatement.executeUpdate();
         System.out.println("Changed: " + changeCount + " fields\n");
-
       }
     } catch (SQLException e) {
-      System.err.println("Couldn't Update Reservation " + reservationCode + ": " + e.getMessage().split(";")[0] + "\n");
+      System.err.println(
+        "Couldn't Update Reservation " +
+        reservationCode +
+        ": " +
+        e.getMessage().split(";")[0] +
+        "\n"
+      );
     }
   }
 
@@ -361,34 +433,49 @@ public class InnReservations {
     try {
       int code = in.nextInt();
 
-      try (Connection conn = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD)) {
+      try (
+        Connection conn = DriverManager.getConnection(
+          JDBC_URL,
+          JDBC_USER,
+          JDBC_PASSWORD
+        )
+      ) {
+        String sqlStatement =
+          "SELECT CODE FROM lab7_reservations WHERE CODE = ?";
 
-        String sqlStatment = "SELECT CODE FROM lab7_reservations WHERE CODE = ?";
-
-        try (PreparedStatement psmt = conn.prepareStatement(sqlStatment, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE)) {
+        try (
+          PreparedStatement psmt = conn.prepareStatement(sqlStatement,
+            ResultSet.TYPE_SCROLL_INSENSITIVE,
+            ResultSet.CONCUR_UPDATABLE
+          )
+        ) {
           psmt.setInt(1, code);
           ResultSet rs = psmt.executeQuery();
-          
+
           if (!rs.next()) {
-            System.out.println("Reservation: " + code + ", Could not be located\n");
+            System.out.println(
+              "Reservation: " + code + ", Could not be located\n"
+            );
             return;
           }
 
           System.out.println("Reservation: " + code + " Found");
-          System.out.println("Confirm Cancelization (Yes/No)?");
+          System.out.println("Confirm Cancellation (Yes/No)?");
           String confirm = in.next();
           if (confirm.toUpperCase().equals("YES")) {
             rs.deleteRow();
-            System.out.println("Reservation: " + code + " Sucessfully Canceled\n");
+            System.out.println(
+              "Reservation: " + code + " Successfully Canceled\n"
+            );
           } else {
             System.out.println("Reservation: " + code + " Was Not Canceled\n");
           }
-
         }
       } catch (SQLException e) {
-        System.out.println("Reservation: " + code + ", Could not be canceled\n");
+        System.out.println(
+          "Reservation: " + code + ", Could not be canceled\n"
+        );
       }
-
     } catch (InputMismatchException e) {
       System.out.println("Reservation Numbers are numbers\n");
     }
@@ -425,27 +512,30 @@ public class InnReservations {
     ) {
       String query =
         "with reservationcost as (\n" +
-        "select *, datediff(day, checkin, checkout) * rate as cost\n" +
-        "from lab7_rooms rooms\n" +
-        "join lab7_reservations rs on rs.room = rooms.roomcode\n" +
+          "select *, datediff(day, checkin, checkout) * rate as cost\n" +
+          "from lab7_rooms rooms\n" +
+          "join lab7_reservations rs on rs.room = rooms.roomcode\n" +
         "), monthrevenue as (\n" +
-        "select roomcode, roomname, month(checkout) as month, sum(cost) monthtotal\n" +
-        "from reservationcost\n" +
-        "group by month(checkout), roomcode\n" +
+          "select roomcode, roomname, month(checkout) as month, sum(cost) monthtotal\n" +
+          "from reservationcost\n" +
+          "group by month(checkout), roomcode\n" +
         ")\n" +
         "select *\n" +
         "from monthrevenue\n" +
         "order by roomcode, month";
 
       try (
-        Statement statement = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        Statement statement = conn.createStatement(
+          ResultSet.TYPE_SCROLL_INSENSITIVE,
+          ResultSet.CONCUR_READ_ONLY
+        );
         ResultSet rs = statement.executeQuery(query)
       ) {
         float[] monthFinalRevenue = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
         float finalRevenue = 0;
 
         System.out.println("Revenue Summary:");
-        System.out.print(heading);
+        System.out.print(heading.toUpperCase());
 
         while (rs.next()) {
           String roomCode = rs.getString("roomcode");
@@ -478,13 +568,13 @@ public class InnReservations {
             "%-4s | %-24s %s %n",
             tempCode,
             roomName,
-            yearFormat(months, total)
+            formatYear(months, total)
           );
         }
         System.out.format(
           "%-31s %s %n",
           "FINAL TOTAL:",
-          yearFormat(monthFinalRevenue, finalRevenue)
+          formatYear(monthFinalRevenue, finalRevenue)
         );
       }
     } catch (SQLException e) {
@@ -494,16 +584,16 @@ public class InnReservations {
     System.out.println();
   }
 
-  private static String yearFormat(float[] totalArr, float total) {
-    StringBuilder totals = new StringBuilder();
+  private static String formatYear(float[] totalArr, float total) {
+    String output = "";
 
-      for (float v : totalArr) {
-          String monthTotal = String.format("$%s", Math.round(v));
-          String padded = String.format("| %-10s", monthTotal);
-          totals.append(padded);
-      }
+    for (float v : totalArr) {
+      String monthTotal = String.format("$%s", Math.round(v));
+      String padded = String.format("| %-10s", monthTotal);
+      output += padded;
+    }
 
     String monthTotal = String.format("$%s", Math.round(total));
-    return totals + String.format("| %-9s", monthTotal);
+    return output + String.format("| %-9s", monthTotal);
   }
 }
