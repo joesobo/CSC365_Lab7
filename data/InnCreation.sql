6492eb4805936bbe311e5362b461fa2162f8396f
Rye Cassan <26152348+hondarider29@users.noreply.github.com>
2021-03-16 19:37:13 -0500

    Double Booking Trigger Connected to Database

--- data/InnCreation.sql
+++ data/InnCreation.sql
@@ -5,7 +5,7 @@ CREATE TABLE lab7_rooms (RoomCode CHAR(5) PRIMARY KEY, RoomName VARCHAR(30), Bed
 CREATE TABLE lab7_reservations (CODE INTEGER PRIMARY KEY, Room CHAR(5), CheckIn DATE, Checkout DATE, Rate FLOAT, LastName VARCHAR(15), FirstName VARCHAR(15), Adults INTEGER, Kids INTEGER, FOREIGN KEY (Room) REFERENCES lab7_rooms (RoomCode));
 
 -- setup constraints
--- CREATE TRIGGER no_double_bookings BEFORE INSERT ON lab7_reservations FOR EACH ROW CALL "database.NoDoubleBookingsTrigger";
--- CREATE TRIGGER no_double_bookings_update BEFORE UPDATE ON lab7_reservations FOR EACH ROW CALL "database.NoDoubleBookingsTrigger";
+CREATE TRIGGER double_bookings BEFORE INSERT ON lab7_reservations FOR EACH ROW CALL "DoubleBookingsTrigger";
+CREATE TRIGGER double_bookings_update BEFORE UPDATE ON lab7_reservations FOR EACH ROW CALL "DoubleBookingsTrigger";
 -- CREATE TRIGGER check_max_occupancy BEFORE INSERT ON lab7_reservations FOR EACH ROW CALL "database.CheckMaxOccTrigger";
 -- CREATE TRIGGER check_max_occupancy_update BEFORE UPDATE ON lab7_reservations FOR EACH ROW CALL "database.CheckMaxOccTrigger";
